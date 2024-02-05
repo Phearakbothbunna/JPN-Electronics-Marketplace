11ccce07bd8924389c6654abbcf368193f137609
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2024-02-04 22:21:03 -0800

    Added skeletal code for project
    Setup react and spring boot (spring initializer)

---
+++ JPN-Electronics-Marketplace/src/main/frontend/src/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import { Container, Form, Button } from "react-bootstrap";
+
+
+function Register() {
+    return (
+        <h1>Registration</h1>
+
+    );
+}
+
+export default Register;

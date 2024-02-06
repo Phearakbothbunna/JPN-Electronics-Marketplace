c56e658ff3c1cde29817d3fc5b2b6b79bfb9506f
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2024-02-06 10:52:31 -0800

    Login/Registration form
    Used react bootstrap for the login/register forms

--- JPN-Electronics-Marketplace/src/main/frontend/src/Login.js
+++ JPN-Electronics-Marketplace/src/main/frontend/src/Login.js
@@ -1,14 +1,36 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import { Container, Form, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Login.css'
 
 
 function Login() {
     return (
-        <div>
-            <p>LOGIN PAGE</p>
-            <Link to="/register">Click here to register!</Link>
-        </div>
+
+        <Container className="mt-5">
+            <Form className="login_form" >
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label style={{color: "white"}}>Email address</Form.Label>
+                    <Form.Control type="text" name="email" placeholder="Enter Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label  style={{color: "white"}}>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Enter Password" />
+                </Form.Group>
+
+                <Button className="btn_login" variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
+
+            <div className="d-flex justify-content-center mt-3">
+                <Link to="/register" className="btn btn-secondary mr-2">Register</Link>
+            </div>
+        </Container>
+
     );
 }
 
--- JPN-Electronics-Marketplace/src/main/frontend/src/Register.js
+++ JPN-Electronics-Marketplace/src/main/frontend/src/Register.js
@@ -5,9 +5,35 @@ import { Container, Form, Button } from "react-bootstrap";
 
 function Register() {
     return (
-        <h1>Registration</h1>
 
-    );
+        <Container className="mt-5">
+            <Form className="login_form" >
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label style={{color: "white"}}>Email address</Form.Label>
+                    <Form.Control type="text" name="email" placeholder="Enter Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label  style={{color: "white"}}>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Enter Password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label  style={{color: "white"}}>Confirm Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Confirm Password" />
+                </Form.Group>
+
+                <Link to="/">
+                    <Button className="btn_register" variant="outline-primary" type="submit">
+                        Submit!
+                    </Button>
+                </Link>
+            </Form>
+        </Container>
+
+
+);
 }
 
 export default Register;

6490f51a3c69da82bfe246330b8d508bea37547b
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2024-02-24 05:33:29 -0800

    Fixed sessions

--- JPN-Electronics-Marketplace/src/main/frontend/src/ProtectedRoute.js
+++ JPN-Electronics-Marketplace/src/main/frontend/src/ProtectedRoute.js
@@ -1,15 +1,17 @@
 import React, {useEffect} from 'react';
-import {Navigate} from "react-router-dom";
+import {Navigate, useLocation} from "react-router-dom";
 
 // This will be used to prevent users from routing to the homepage without authentication
 function ProtectedRoute ({children}) {
+    const location = useLocation();
     // This will ensure that the session will be cleared whenever user clicks on the back button (moving away from homepage)
     useEffect(() => {
-        // Clear session when component unmounts
-        return () => {
-            sessionStorage.removeItem('user');
-        };
-    }, []);
+        // Clear session
+        if (location.pathname === "/login")
+            return () => {
+                sessionStorage.removeItem('user');
+            };
+    }, [location]);
     const userData = JSON.parse(sessionStorage.getItem("user"));
     if (!userData) {
         return <div>Please <a href={"/login"}> log in </a> to access this page</div>;
